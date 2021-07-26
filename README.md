1046dcc66eda06865f20e85a4a2b3c408f7f29b1
June-Ruth <n-barral@hotmail.fr>
2021-07-26 11:06:39 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+#Mediscreen
+##Abernathy Clinic
---
+++ build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    id("org.springframework.boot") version("2.5.3")
+    id("io.spring.dependency-management") version("1.0.11.RELEASE")
+    java
+    jacoco
+}
+
+group = "com.openclassrooms"
+version = "0.0.1-SNAPSHOT"
+
+
+afterEvaluate {
+    java {
+        toolchain {
+            languageVersion.set(JavaLanguageVersion.of(11))
+        }
+    }
+
+    jacoco {
+        toolVersion = "0.8.7"
+    }
+
+    tasks.test {
+        useJUnitPlatform()
+        finalizedBy(tasks.jacocoTestReport)
+    //exclude("**/*IT.class")
+    }
+
+    tasks.jacocoTestReport {
+        dependsOn(tasks.test)
+        reports {
+            xml.required.set(true)
+            csv.required.set(false)
+            html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml").get().asFile)
+        }
+    }
+}
+
+    repositories {
+        mavenCentral()
+    }
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    //implementation("org.springframework.boot:spring-boot-starter-data-mongodb") => for NoSQL DB for notes
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    runtimeOnly("mysql:mysql-connector-java")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
